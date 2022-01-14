8730440b14240282cb62c8c2a66c3f1a6390f7f2
RamanaKatikela <murthy.ram30@gmail.com>
2022-01-14 14:43:42 -0500

    First comit

---
+++ src/com/ram/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.ram;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is First on new Laptop");
+	}
+
+}
